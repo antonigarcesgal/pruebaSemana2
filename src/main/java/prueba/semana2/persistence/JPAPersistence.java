9d6db8bf800aeda7c71dde87e1efb5315b6403a6
Admin <Admin@10.250.5.6>
2023-04-24 16:16:21 +0200

    Añadido persistence.xml, que permite la conexion a la base de datos, y arreglada la clase JPAPersistence para utilizar este.

--- src/main/java/prueba/semana2/persistence/JPAPersistence.java
+++ src/main/java/prueba/semana2/persistence/JPAPersistence.java
@@ -3,6 +3,31 @@ package prueba.semana2.persistence;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * JPAPersistence.java
+ * 
+ * @autor: Antoni
+ * @version: 1.0
+ */
 public class JPAPersistence {
-	
+	// Utilizando el persistence.xml en la carpeta META_INF definimos los parametros
+	// necesarios para conectarse con la base de datos
+	private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
+
+	private static EntityManagerFactory factory;
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+
+		return factory;
+	}
+
+	//Cierra la conexión establecida
+	public static void shutdown() {
+		if (factory != null) {
+			factory.close();
+		}
+	}
 }
